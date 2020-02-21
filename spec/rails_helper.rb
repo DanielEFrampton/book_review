a7e5edb17c8d2f95159e6cabd896eef0534bbdb9
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-21 10:44:07 -0700

    Configure VCR and create cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,8 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
+require 'vcr'
+
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
@@ -60,4 +62,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
